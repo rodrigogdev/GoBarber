46a9aeaf56daedfd053ca354795493d97ad2fedb
Rodrigo <rodrigogoncalves121@gmail.com>
2022-03-31 22:22:25 -0300

    Fixed some bugs on tests

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -38,6 +38,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server started at port 3333!");
 });
